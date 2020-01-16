98954df1a7d9a773a975acf8c945c8d5485a53a0
Namita <you@example.com>
2020-01-16 12:16:30 +0530

    Added Preview component

---
+++ FormlyForms/src/app/preview-form/preview-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PreviewFormComponent } from './preview-form.component';
+
+
+
+@NgModule({
+  declarations: [PreviewFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PreviewFormModule { }
